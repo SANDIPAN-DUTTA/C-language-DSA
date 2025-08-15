05af5d080df69bf4e50c20de807f2d526d819eca
Sandipan Dutta <duttasandipan15@gmail.com>
2025-08-15 11:51:11 +0530

    Insert First fuunction in a single linked list

---
+++ single_linkedlist/insertfirst.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data; 
+    struct Node *next;
+};
+
+struct Node* insertfirst(struct Node *head) {
+    struct Node *newnode;
+    newnode = (struct Node *)malloc(sizeof(struct Node));
+    printf("\n Enter new node data at beginning: ");
+    scanf("%d", &newnode->data);
+    newnode->next = head;
+    head = newnode;
+    return head;
+}
+
+void display(struct Node *head) {
+    struct Node *ptr;
+    ptr = head;
+    printf("\n Linked List: ");
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL \n");
+}
+
+
+int main() {
+    struct Node *head = NULL;
+
+    for (int i = 0; i < 3; i++) {
+        head = insertfirst(head);
+    }
+    display(head);
+    return 0;
+}
